6d3168f1c0b96d574bc2d78731ed042be2939be0
David Driscoll <david.driscoll@gmail.com>
2016-08-14 01:00:11 -0400

    Added index.js in

---
+++ index.js
@@ -0,0 +1,8 @@
+var fs = require('fs'), path = require('path');
+
+// Allow us to deploy a dist version... but locally not worry about it
+if (fs.existsSync(path.resolve(__dirname, 'src/index.js'))) {
+    module.exports = require(path.join(__dirname, 'src/index'));
+} else {
+    module.exports = require(path.join(__dirname, 'dist/index'));
+}
